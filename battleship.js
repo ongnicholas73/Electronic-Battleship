04579c9dd4b0e7aa2df7d6c13f365ae1c1a6f76b
ongnicholas73 <ongnicholas73@gmail.com>
2022-08-21 16:04:34 -0700

    added grid

---
+++ battleship.js
@@ -0,0 +1,18 @@
+console.log("******** script is running");
+
+const shipData = {
+  direction: 0,
+  startLocation: {
+    row: "A",
+    col: "1",
+  },
+  length: 6,
+};
+
+const generateShip = (shipData) => {
+  if (shipData.direction === 0) {
+    for (let i = 0; i < array.length; i++) {
+      document.querySelector(`.cell-row#${shipData.startLocation}`);
+    }
+  }
+};
